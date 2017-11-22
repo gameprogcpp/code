481d2c71206eca66ce4b59cb74368c4446b646d6
Sanjay Madhav <madhav@usc.edu>
2017-11-22 00:42:16 -0800

    Chapter 3 done (windows)

--- Chapter03/Ship.cpp
+++ Chapter03/Ship.cpp
@@ -17,7 +17,7 @@ Ship::Ship(Game* game)
 	,mLaserCooldown(0.0f)
 {
 	// Create a sprite component
-	SpriteComponent* sc = new SpriteComponent(this);
+	SpriteComponent* sc = new SpriteComponent(this, 150);
 	sc->SetTexture(game->GetTexture("Assets/Ship.png"));
 
 	// Create an input component and set keys/speed
@@ -39,7 +39,7 @@ void Ship::ActorInput(const uint8_t* keyState)
 {
 	if (keyState[SDL_SCANCODE_SPACE] && mLaserCooldown <= 0.0f)
 	{
-		// Create a laser and set it's position/rotation to mine
+		// Create a laser and set its position/rotation to mine
 		Laser* laser = new Laser(GetGame());
 		laser->SetPosition(GetPosition());
 		laser->SetRotation(GetRotation());
