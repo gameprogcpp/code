eca5ff04e7d51cca1467b03b18fba673b99b3a09
chalonverse <sanjay12@gmail.com>
2018-01-16 15:58:45 -0800

    Fixed window title for chapter 8

--- Chapter08/Game.cpp
+++ Chapter08/Game.cpp
@@ -52,7 +52,7 @@ bool Game::Initialize()
 	// Force OpenGL to use hardware acceleration
 	SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
 	
-	mWindow = SDL_CreateWindow("Game Programming in C++ (Chapter 5)", 100, 100,
+	mWindow = SDL_CreateWindow("Game Programming in C++ (Chapter 8)", 100, 100,
 							   1024, 768, SDL_WINDOW_OPENGL);
 	if (!mWindow)
 	{
